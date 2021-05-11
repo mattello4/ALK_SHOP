6c01524a43086b514c2a340456c6fa1a78dad497
Mateusz Paszkiewicz <mateusz.paszkiewicz@onet.pl>
2021-05-11 14:38:32 +0200

    first commit

--- src/views/rooms/outofdate.js
+++ src/views/rooms/outofdate.js
@@ -2,7 +2,7 @@ export const outofdate = (value1, value2) => {
   if (typeof value1 === "string" && typeof value2 === "string") {
     var value1 = new Date(value1);
     var value2 = new Date(value2);
-    return (value2) => value1;
+    return value2 > value1;
   }
   return true;
 };
